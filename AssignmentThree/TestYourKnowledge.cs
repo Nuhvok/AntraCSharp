934d035f42cf087a12c485c5153b97d00b83b2c4
128968 <07161668>
2021-11-04 22:16:34 -0400

    know

---
+++ AssignmentThree/TestYourKnowledge.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentThree
+{
+    class TestYourKnowledge
+    {
+        /************************************************************
+         * 
+         * (1 -- Public: This is available to basically everywhere
+         *    -- Private: This is available to only members within
+         *          the class
+         *    -- Protected: This is available to members within the
+         *          class and all derived members
+         *    -- Private Protected: This is available to members 
+         *          within the class and derived members who are in
+         *          the same assembly
+         *    -- Internal: This is available to members within the
+         *          class and those within the same assembly
+         *    -- Protected Internal: This is available to members
+         *          within the class, derived members inside and
+         *          outside the assembly, and non-derived from
+         *          outside the assembly
+         * (2 -- Static: A static type member will be shared across
+         *          every instance of whatever the static type
+         *          member is a part of
+         *    -- Const: a constant type member is an unchangable
+         *          member
+         *    -- Readonly: A readonly type member can only be set 
+         *          by a constructor. After that, it can only be read
+         * (3 A constructor initializes the fields of an object when 
+         *      it is created.
+         * (4  The partial keyword allows a class to be broken up into
+         *      multiple files. This allows the class to be worked on
+         *      by more than one person without allowing for
+         *      intersections/overwrites
+         * (5 A tuble is an array like struct which is used to pass
+         *      multiple pieces of data with one variable
+         * (6
+         * (7 Overloading allows multiple methods with the same name
+         *      and different parameters to exist. The appropriate
+         *      method is chosen depending on the parameters passed
+         * (8 A field is a literal memory location that either holds
+         *      data or a reference to an object. A property is a 
+         *      construct that manages the getting and setting of a 
+         *      field
+         * (9 Optional parameters are preinitialized. The method
+         *      prototype would look something like this:
+         *      Method(int data = 10)
+         * (10 An interface is a collection of method prototypes
+         *      that must be implemented by the implementing class.
+         *      Abstract classes have similar collections of
+         *      prototypes, but they can also include data fields
+         *      and must be inherited, not implemented.
+         * (11 Public
+         * (12 True
+         * (13 True
+         * (14 False
+         * (15 True
+         * (16 False
+         * (17 True
+         * (18 True
+         * (19 False
+         * (20 False
+         * (21 True
+         * (22 False
+         * (23 True
+         * 
+         * 
+         *************************************************************/
+    }
+}

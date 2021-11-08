d20fac582e064eb93d31b5ad6c099936dc619442
128968 <07161668>
2021-11-08 01:00:55 -0500

    assignment 4 started

---
+++ AssignmentFour/MyStack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentFour
+{
+    class MyStack <T>
+    {
+        Stack<T> stack;
+
+        public MyStack()
+        {
+            this.stack = new Stack<T>();
+        }
+
+        public int Count()
+        {
+            return stack.Count;
+        }
+
+        public T Pop()
+        {
+            return stack.Pop();
+        }
+
+        public void Push(T value)
+        {
+            stack.Push(value);
+        }
+    }
+}
---
+++ AssignmentFour/TestYourKnowledge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentFour
+{
+    class TestYourKnowledge
+    {
+
+        /************************************************************
+         * 
+         * (1 
+         * (2 new List<string>()
+         * (3 Two
+         * (4 False
+         * (5 Add()
+         * (6 RemoveAt(),Remove()
+         * (7 <T>
+         * (8 False
+         * (9 True
+         * (10 True
+         * 
+         *************************************************************/
+    }
+}

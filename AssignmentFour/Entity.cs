405adaf29b8bad371dd60e540a740cea7b27808f
128968 <07161668>
2021-11-09 11:37:42 -0500

    repo done

---
+++ AssignmentFour/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentFour
+{
+    class Entity
+    {
+        int id;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public override string ToString()
+        {
+            return "id:   " + id;
+        }
+    }
+}
---
+++ AssignmentFour/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentFour
+{
+    class GenericRepository<T> : IRepository<T> 
+        where T : Entity
+    {
+        List<T> data = new List<T>();
+
+        public void Add(T item)
+        {
+            data.Add(item);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return data;
+        }
+
+        public T GetById(int id)
+        {
+            foreach(T i in data)
+            {
+                if(i.Id == id)
+                {
+                    return i;
+                }
+                
+            }
+            return null;
+            
+        }
+
+        public void Remove(T item)
+        {
+            data.Remove(item);
+        }
+
+        public void Save()
+        {
+            StringBuilder sb = new StringBuilder("Printout for GenericRepository:\n\n\n");
+
+            foreach(T i in data)
+            {
+                sb.Append(i.ToString() + "\n");
+            }
+
+            File.WriteAllTextAsync("RepoOutput.txt", sb.ToString());
+        }
+    }
+}
